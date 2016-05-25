6e02c04092de718ca260bd610c09b0fb410b05a0
Mikhail Sapozhnikov <mikhailsapozhnikov@Mikhails-MacBook-Pro.local>
2016-05-25 07:48:50 -0400

    Fix binary search.

--- Program.cs
+++ Program.cs
@@ -11,17 +11,15 @@ namespace Practice
         static void Main(string[] args)
         {
             int[] arr = new int[10] { 0, 6, 8, 11, 12, 13, 14, 20, 22, 27 };
-            Random r = new Random();
 
-
-            Sort.quickSort(ref arr, 0, arr.Length - 1);
+            // Uncomment below to test quickSort and specify unsorted array above
+            //Sort.quickSort(ref arr, 0, arr.Length - 1);
 
             for (int i = 0; i < arr.Length; i++) Console.Write("{0} ", arr[i]);
             Console.WriteLine();
+
+            // Binary Search
             Console.WriteLine("The number {0} is at index {1}.", 11, Search.binarySearch(11, arr, 0, arr.Length - 1));
-
-
-        
         }
 
     }
--- Search.cs
+++ Search.cs
@@ -1,25 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice
 {
     class Search
     {
+        /// <summary>
+        /// Performs a binary search to locate the index of a given argument in an array.
+        /// </summary>
+        /// <param name="searchItem">The item to search for.</param>
+        /// <param name="arr">The array which will be searched through.</param>
+        /// <param name="start">The start index at which to perform search.</param>
+        /// <param name="end">The end index at which to perform search.</param>
+        /// <returns>Index of the searchItem in arr if located, or -1 if the item is not found.</returns>
         public static int binarySearch(int searchItem, int[] arr, int start, int end)
         {
-            if (arr.Length == 0) return -1;
-            else if (arr.Length == 1)  return (arr[0] == searchItem) ? 0 : -1;
+            // We've exceeded the array on the right side and searchItem has not been found.
+            if (start > end)
+            {
+                return -1;
+            }
+            // Both are pointing to the same item, or there are no items in the array.
+            else if (end == start)
+            {
+                return (arr[end] == searchItem) ? 0 : -1;
+            }
+            // Otherwise, we perform a recursive binary search.
             else
             {
                 int mid = (end - start + 1) / 2 + start;
                 int index = -1;
 
+                // Midpoint landed on the searchItem, so we return it.
                 if (arr[mid] == searchItem) return mid;
-                else if (start == end) return -1;
+                // searchItem may only be located to the LEFT of the midpoint.
                 else if (arr[mid] > searchItem) index = binarySearch(searchItem, arr, start, mid - 1);
+
+                // searchItem may only be located to the RIGHT of the midpoint.
                 else index = binarySearch(searchItem, arr, mid + 1, end);
 
                 return index;
